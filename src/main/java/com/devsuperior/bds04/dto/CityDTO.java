df9a724769d8e0bd4efcbe40fd04debe5a1de89a
Douglas <douglasantana007@gmail.com>
2022-12-22 23:37:43 -0300

    [ADD] Validation.

--- src/main/java/com/devsuperior/bds04/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityDTO.java
@@ -4,38 +4,41 @@ import java.io.Serializable;
 
 import com.devsuperior.bds04.entities.City;
 
+import javax.validation.constraints.NotBlank;
+
 public class CityDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	private Long id;
-	private String name;
-	
-	public CityDTO() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public CityDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public CityDTO(City entity) {
-		id = entity.getId();
-		name = entity.getName();
-	}
+    private Long id;
+    @NotBlank(message = "Campo requerido")
+    private String name;
 
-	public Long getId() {
-		return id;
-	}
+    public CityDTO() {
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public CityDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public CityDTO(City entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
