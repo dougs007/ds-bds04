f5f71408909017365d55cab92f2c31db91551c45
Douglas <douglasantana007@gmail.com>
2022-12-22 23:35:27 -0300

    [ADD] City endpoint.
    - Service, Entity and repository created.

--- src/main/java/com/devsuperior/bds04/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -1,10 +1,37 @@
 package com.devsuperior.bds04.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.services.CityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
 public class CityController {
 
+    private final CityService service;
+
+    public CityController(CityService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
+        CityDTO city = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(city);
+    }
 }
---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    static final String CITY_NOT_FOUND = "City not found";
+    static final String DATA_INTEGRITY_VIOLATION_EXCEPTION = "Data Integrity Violation";
+
+    private final CityRepository repository;
+
+    public CityService(CityRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> result = repository.findAll(Sort.by("name"));
+        return result.stream().map(CityDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City(null, dto.getName());
+        entity = repository.save(entity);
+
+        return new CityDTO(entity);
+    }
+}
